1b19d0bdb34bd73a24ede60ea3635c3f2f002ae2
Aerdalis01 <caillonaudric@hotmail.fr>
2024-06-12 14:58:38 +0200

    ajout de la page services

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,10 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "accueil", "/pages/home.html"),
+    new Route("/habitats", "Habitats", "/pages/habitats.html"),
+    new Route("/services", "Services", "/pages/services.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Zoo Arcadia";
