bc2409be88a37991928f8fc526730bce84062a7c
Aerdalis01 <caillonaudric@hotmail.fr>
2024-06-20 11:31:10 +0200

    Modifications des images habitats et _animaux Modifications des images du carousel et des images de la page habitats Ajout des routes et pages savane / marais / jungle

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,6 +6,9 @@ export const allRoutes = [
     new Route("/habitats", "Habitats", "/pages/habitats.html"),
     new Route("/services", "Services", "/pages/services.html"),
     new Route("/contact", "Contact", "/pages/contact.html"),
+    new Route("/marais", "Marais", "/pages/marais.html"),
+    new Route("/savane", "Savane", "/pages/savane.html"),
+    new Route("/jungle", "Jungle", "/pages/jungle.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
