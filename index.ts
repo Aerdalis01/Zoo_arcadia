b8daa48edc966e96eef48d87c073463381830c1d
Aerdalis01 <caillonaudric@hotmail.fr>
2024-06-20 14:54:59 +0200

    Initialisation des variables

---
+++ index.ts
@@ -0,0 +1,22 @@
+//Déclaration des variables
+
+const menuBurger = document.querySelector('.container_menu-burger') as HTMLElement
+
+const modalMenu = document.querySelector('.modal-menu') as HTMLElement
+
+const modalMenuExit = document.querySelector('.content-exit') as HTMLElement
+
+
+const iconeUser = document.querySelector('.container_icone-user') as HTMLElement
+
+const connexion = document.querySelector('.connexion') as HTMLElement
+
+const connexionExit = document.querySelector('.content-exit') as HTMLElement
+
+//au clic sur le bouton "avis-button" la modal "modal-form" apparaît, au clic sur la 
+const avisBtn = document.querySelector('.avis-button') as HTMLElement
+console.log(avisBtn)
+
+const modalAvis = document.querySelector('.modal-form') as HTMLElement
+console.log(modalAvis)
+
